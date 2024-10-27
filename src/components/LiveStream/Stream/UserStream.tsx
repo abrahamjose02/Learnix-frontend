ede0f2fcdd5d8173ae93c22efee09beeaddf4ba2
Abraham Jose <abrahamjose002@gmail.com>
2024-10-27 17:51:46 +0530

    caller tokens updated

--- src/components/LiveStream/Stream/UserStream.tsx
+++ src/components/LiveStream/Stream/UserStream.tsx
@@ -8,8 +8,8 @@ import {
 import "@stream-io/video-react-sdk/dist/css/styles.css";
 
 const apiKey = 'mmhfdzb5evj2';
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL05vbV9Bbm9yIiwidXNlcl9pZCI6Ik5vbV9Bbm9yIiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6NjA0ODAwLCJpYXQiOjE3Mjc1ODg2MjIsImV4cCI6MTcyODE5MzQyMn0.B2qOPCCg9oCDkzhWPv5t5re19gw9E0B5_6OUs0TEQw4';
-const userId = 'Nom_Anor';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL1F1aW5sYW5fVm9zIiwidXNlcl9pZCI6IlF1aW5sYW5fVm9zIiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6NjA0ODAwLCJpYXQiOjE3MzAwMzEzMzIsImV4cCI6MTczMDYzNjEzMn0.ozOo_dpJuGNea1BiN7UDyjDCb4V33hP40S6llk0CTL0';
+const userId = 'Quinlan_Vos';
 
 const client = new StreamVideoClient({
   apiKey,
